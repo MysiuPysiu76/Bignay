69d716d9c69e0b679fdaf1a88f68014fb976e583
Michał Hrycek <kotyk9921@gmail.com>
2025-06-19 23:02:26 +0000

    Mod init
    Mod init

---
+++ src/main/java/com/mysiupysiu/bignay/BignayMod.java
@@ -0,0 +1,11 @@
+package com.mysiupysiu.bignay;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("bignay")
+public class BignayMod {
+    public static final String MODID = "bignay";
+
+    public BignayMod() {
+    }
+}
