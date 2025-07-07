63a30313e6adee3ac7b0ba2217c46419ebdb60aa
Michał Hrycek <kotyk9921@gmail.com>
2025-07-07 18:18:32 +0200

    Added cracked deepslate bricks variants: slabs and stairs
    Added cracked deepslate bricks variants: slabs and stairs

--- src/main/java/com/mysiupysiu/bignay/blocks/BlockInit.java
+++ src/main/java/com/mysiupysiu/bignay/blocks/BlockInit.java
@@ -76,6 +76,8 @@ public class BlockInit {
 
     public static final RegistryObject<Block> CRACKED_STONE_BRICK_STAIRS = registerBlock("cracked_stone_brick_stairs", StairsBlock::new);
     public static final RegistryObject<Block> CRACKED_STONE_BRICK_SLAB = registerBlock("cracked_stone_brick_slab", SlabsBlock::new);
+    public static final RegistryObject<Block> CRACKED_DEEPSLATE_BRICK_STAIRS = registerBlock("cracked_deepslate_brick_stairs", StairsBlock::new);
+    public static final RegistryObject<Block> CRACKED_DEEPSLATE_BRICK_SLAB = registerBlock("cracked_deepslate_brick_slab", SlabsBlock::new);
 
     private static RegistryObject<Block> registerBlock(String name, Supplier<Block> blockSupplier) {
         RegistryObject<Block> block = BLOCKS.register(name, blockSupplier);
