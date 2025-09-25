5096250bb1e889da736a5965c02d09c4ea78d150
Michał Hrycek <kotyk9921@gmail.com>
2025-09-25 17:51:28 +0200

    Created Bamboo Block Fence
    Created Bamboo Block Fence

--- src/main/java/com/mysiupysiu/bignay/blocks/BlockInit.java
+++ src/main/java/com/mysiupysiu/bignay/blocks/BlockInit.java
@@ -255,6 +255,7 @@ public class BlockInit {
     public static final RegistryObject<Block> MOSSY_STONE_WALL = registerBlock("mossy_stone_wall", WallsBlock::new);
 
     public static final RegistryObject<Block> ACACIA_LOG_FENCE = registerBlock("acacia_wood_fence", () -> new FenceBlock(BlockBehaviour.Properties.copy(Blocks.ACACIA_FENCE)));
+    public static final RegistryObject<Block> BAMBOO_BLOCK_FENCE = registerBlock("bamboo_block_fence", () -> new FenceBlock(BlockBehaviour.Properties.copy(Blocks.BIRCH_FENCE)));
     public static final RegistryObject<Block> BIRCH_LOG_FENCE = registerBlock("birch_wood_fence", () -> new FenceBlock(BlockBehaviour.Properties.copy(Blocks.BIRCH_FENCE)));
     public static final RegistryObject<Block> CHERRY_LOG_FENCE = registerBlock("cherry_wood_fence", () -> new FenceBlock(BlockBehaviour.Properties.copy(Blocks.CHERRY_FENCE)));
 
