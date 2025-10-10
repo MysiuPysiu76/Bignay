382da2d29e6cb16a00cdd5bc3ea2a59e481f5cc7
Michał Hrycek <kotyk9921@gmail.com>
2025-10-10 20:53:46 +0200

    Created Jungle Mosaic
    Created Jungle Mosaic

--- src/main/java/com/mysiupysiu/bignay/blocks/BlockInit.java
+++ src/main/java/com/mysiupysiu/bignay/blocks/BlockInit.java
@@ -301,6 +301,7 @@ public class BlockInit {
     public static final RegistryObject<Block> CHERRY_MOSAIC = registerBlock("cherry_mosaic", () -> new Block(BlockBehaviour.Properties.copy(Blocks.CHERRY_PLANKS)));
     public static final RegistryObject<Block> CRIMSON_MOSAIC = registerBlock("crimson_mosaic", () -> new Block(BlockBehaviour.Properties.copy(Blocks.CRIMSON_PLANKS)));
     public static final RegistryObject<Block> DARK_OAK_MOSAIC = registerBlock("dark_oak_mosaic", () -> new Block(BlockBehaviour.Properties.copy(Blocks.DARK_OAK_PLANKS)));
+    public static final RegistryObject<Block> JUNGLE_MOSAIC = registerBlock("jungle_mosaic", () -> new Block(BlockBehaviour.Properties.copy(Blocks.JUNGLE_PLANKS)));
 
     private static RegistryObject<Block> registerBlock(String name, Supplier<Block> blockSupplier) {
         RegistryObject<Block> block = BLOCKS.register(name, blockSupplier);
