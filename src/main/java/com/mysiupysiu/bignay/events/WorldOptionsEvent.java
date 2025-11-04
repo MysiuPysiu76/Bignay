d6130f127a9acba6ff79abcc1182a3f1baad15e0
Michał Hrycek <kotyk9921@gmail.com>
2025-11-04 21:04:28 +0100

    Added Re-Create Button to Edit World Screen
    Added Re-Create Button to Edit World Screen

--- src/main/java/com/mysiupysiu/bignay/events/WorldOptionsEvent.java
+++ src/main/java/com/mysiupysiu/bignay/events/WorldOptionsEvent.java
@@ -1,18 +1,29 @@
 package com.mysiupysiu.bignay.events;
 
+import com.mojang.datafixers.util.Pair;
 import com.mysiupysiu.bignay.screen.WorldExportScreen;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.events.GuiEventListener;
+import net.minecraft.client.gui.screens.AlertScreen;
+import net.minecraft.client.gui.screens.ConfirmScreen;
+import net.minecraft.client.gui.screens.SymlinkWarningScreen;
+import net.minecraft.client.gui.screens.worldselection.CreateWorldScreen;
 import net.minecraft.client.gui.screens.worldselection.EditWorldScreen;
+import net.minecraft.client.gui.screens.worldselection.WorldCreationContext;
+import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.level.LevelSettings;
+import net.minecraft.world.level.storage.LevelResource;
 import net.minecraft.world.level.storage.LevelStorageSource;
+import net.minecraft.world.level.validation.ContentValidationException;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.ScreenEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 import java.lang.reflect.Field;
+import java.nio.file.Path;
 
 @Mod.EventBusSubscriber(modid = "bignay", bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
 public class WorldOptionsEvent {
@@ -49,5 +60,55 @@ public class WorldOptionsEvent {
         ).bounds(centerX - 100, baseY, buttonWidth, buttonHeight).build();
 
         event.addListener(exportButton);
+
+        Minecraft mc = Minecraft.getInstance();
+
+        int x = screen.width / 2 - 100;
+        int width = 200;
+        int height = 20;
+
+        Button.Builder access = Button.builder(Component.translatable("selectWorld.recreate"), b -> {
+            try {
+                var levelAccessField = EditWorldScreen.class.getDeclaredField("levelAccess");
+                levelAccessField.setAccessible(true);
+                LevelStorageSource.LevelStorageAccess levelAccess =
+                        (LevelStorageSource.LevelStorageAccess) levelAccessField.get(screen);
+
+                Pair<LevelSettings, WorldCreationContext> pair =
+                        mc.createWorldOpenFlows().recreateWorldData(levelAccess);
+                LevelSettings levelSettings = pair.getFirst();
+                WorldCreationContext worldContext = pair.getSecond();
+
+                Path path = CreateWorldScreen.createTempDataPackDirFromExistingWorld(
+                        levelAccess.getLevelPath(LevelResource.DATAPACK_DIR), mc);
+
+                if (worldContext.options().isOldCustomizedWorld()) {
+                    mc.setScreen(new ConfirmScreen((confirm) -> {
+                        if (confirm) {
+                            mc.setScreen(CreateWorldScreen.createFromExisting(mc, screen, levelSettings, worldContext, path));
+                        } else {
+                            mc.setScreen(screen);
+                        }
+                    },
+                            Component.translatable("selectWorld.recreate.customized.title"),
+                            Component.translatable("selectWorld.recreate.customized.text"),
+                            CommonComponents.GUI_PROCEED, CommonComponents.GUI_CANCEL));
+                } else {
+                    mc.setScreen(CreateWorldScreen.createFromExisting(mc, screen, levelSettings, worldContext, path));
+                }
+
+            } catch (ContentValidationException e) {
+                mc.setScreen(new SymlinkWarningScreen(screen));
+            } catch (Exception e) {
+                e.printStackTrace();
+                mc.setScreen(new AlertScreen(() -> mc.setScreen(screen),
+                        Component.translatable("selectWorld.recreate.error.title"),
+                        Component.translatable("selectWorld.recreate.error.text")));
+            }
+        });
+        access.bounds(x, baseY + 24, width, height);
+        Button recreateButton = access.build();
+
+        event.addListener(recreateButton);
     }
 }
