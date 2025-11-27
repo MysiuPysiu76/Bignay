da930588b11ff843e7a3a2d03acadd00fc1dfe84
Michał Hrycek <kotyk9921@gmail.com>
2025-11-27 23:11:19 +0100

    Created Cracked Smooth Basalt Bricks
    Created Cracked Smooth Basalt Bricks

--- src/main/java/com/mysiupysiu/bignay/blocks/BlockInit.java
+++ src/main/java/com/mysiupysiu/bignay/blocks/BlockInit.java
@@ -400,6 +400,10 @@ public class BlockInit {
     public static final RegistryObject<Block> SMOOTH_BASALT_BRICK_SLAB = registerBlock("smooth_basalt_brick_slab", () -> new SlabsBlock(Blocks.SMOOTH_BASALT));
     public static final RegistryObject<Block> SMOOTH_BASALT_BRICK_STAIRS = registerBlock("smooth_basalt_brick_stairs", () -> new StairsBlock(Blocks.SMOOTH_BASALT));
     public static final RegistryObject<Block> SMOOTH_BASALT_BRICK_WALL = registerBlock("smooth_basalt_brick_wall", () -> new WallsBlock(Blocks.SMOOTH_BASALT));
+    public static final RegistryObject<Block> CRACKED_SMOOTH_BASALT_BRICKS = registerBlock("cracked_smooth_basalt_bricks", () -> new BuildingBlock(Blocks.SMOOTH_BASALT));
+    public static final RegistryObject<Block> CRACKED_SMOOTH_BASALT_BRICK_SLAB = registerBlock("cracked_smooth_basalt_brick_slab", () -> new SlabsBlock(Blocks.SMOOTH_BASALT));
+    public static final RegistryObject<Block> CRACKED_SMOOTH_BASALT_BRICK_STAIRS = registerBlock("cracked_smooth_basalt_brick_stairs", () -> new StairsBlock(Blocks.SMOOTH_BASALT));
+    public static final RegistryObject<Block> CRACKED_SMOOTH_BASALT_BRICK_WALL = registerBlock("cracked_smooth_basalt_brick_wall", () -> new WallsBlock(Blocks.SMOOTH_BASALT));
 
     private static RegistryObject<Block> registerBlock(String name, Supplier<Block> blockSupplier) {
         RegistryObject<Block> block = BLOCKS.register(name, blockSupplier);
