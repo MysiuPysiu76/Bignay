e80c87e2cbbd7c63b7047f2f1e8b5c7ceca4a3b8
Michał Hrycek <kotyk9921@gmail.com>
2025-12-01 18:39:48 +0100

    Created Smooth Calcite Pillar
    Created Smooth Calcite Pillar

--- src/main/java/com/mysiupysiu/bignay/blocks/BlockInit.java
+++ src/main/java/com/mysiupysiu/bignay/blocks/BlockInit.java
@@ -453,6 +453,7 @@ public class BlockInit {
     public static final RegistryObject<Block> CRACKED_SMOOTH_CALCITE_BRICK_STAIRS = registerBlock("cracked_smooth_calcite_brick_stairs", () -> new StairsBlock(Blocks.CALCITE));
     public static final RegistryObject<Block> CRACKED_SMOOTH_CALCITE_BRICK_WALL = registerBlock("cracked_smooth_calcite_brick_wall", () -> new WallsBlock(Blocks.CALCITE));
     public static final RegistryObject<Block> CHISELED_SMOOTH_CALCITE = registerBlock("chiseled_smooth_calcite", () -> new BuildingBlock(Blocks.CALCITE));
+    public static final RegistryObject<Block> SMOOTH_CALCITE_PILLAR = registerBlock("smooth_calcite_pillar", () -> new RotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.CALCITE)));
 
     private static RegistryObject<Block> registerBlock(String name, Supplier<Block> blockSupplier) {
         RegistryObject<Block> block = BLOCKS.register(name, blockSupplier);
