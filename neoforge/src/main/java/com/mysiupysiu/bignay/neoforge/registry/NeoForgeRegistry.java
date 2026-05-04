c597c3f8107dd29f44c658544167bc656f338a1b
Michał Hrycek <kotyk9921@gmail.com>
2026-05-04 23:55:38 +0200

    Add Neoforge sounds and instruments registry
    Add Neoforge sounds and instruments registry

--- neoforge/src/main/java/com/mysiupysiu/bignay/neoforge/registry/NeoForgeRegistry.java
+++ neoforge/src/main/java/com/mysiupysiu/bignay/neoforge/registry/NeoForgeRegistry.java
@@ -2,16 +2,21 @@ package com.mysiupysiu.bignay.neoforge.registry;
 
 import com.mysiupysiu.bignay.BignayMod;
 import com.mysiupysiu.bignay.registry.Registrar;
-import com.mysiupysiu.bignay.registry.init.BignayBlockEntities;
-import com.mysiupysiu.bignay.registry.init.BignayBlocks;
-import com.mysiupysiu.bignay.registry.init.BignayEntities;
-import com.mysiupysiu.bignay.registry.init.BignayItems;
+import com.mysiupysiu.bignay.registry.init.*;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Instrument;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
@@ -21,12 +26,18 @@ public class NeoForgeRegistry {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(BignayMod.MODID);
     public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES = DeferredRegister.create(Registries.BLOCK_ENTITY_TYPE, BignayMod.MODID);
     public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(Registries.ENTITY_TYPE, BignayMod.MODID);
+    public static final DeferredRegister<SoundEvent> SOUNDS = DeferredRegister.create(Registries.SOUND_EVENT, BignayMod.MODID);
+    public static final DeferredRegister<Instrument> INSTRUMENTS = DeferredRegister.create(Registries.INSTRUMENT, BignayMod.MODID);
 
     public static void register(IEventBus bus) {
         block(bus);
         blockEntities(bus);
         item(bus);
         entities(bus);
+        sounds(bus);
+        instruments(bus);
+
+        bus.addListener(NeoForgeRegistry::onBuildCreativeTabs);
     }
 
     private static void block(IEventBus bus) {
@@ -59,5 +70,40 @@ public class NeoForgeRegistry {
             entry.setSupplier(obj);
         }
         ENTITY_TYPES.register(bus);
+    }
+
+    private static void sounds(IEventBus bus) {
+        for (Registrar.Entry<SoundEvent> entry : BignaySounds.SOUNDS.getEntries()) {
+            DeferredHolder<SoundEvent, SoundEvent> obj = SOUNDS.register(entry.getId(), entry.getSupplier());
+            entry.setSupplier(obj);
+        }
+        SOUNDS.register(bus);
+    }
+
+    private static void instruments(IEventBus bus) {
+        for (Registrar.Entry<Instrument> entry : BignayInstruments.INSTRUMENTS.getEntries()) {
+            DeferredHolder<Instrument, Instrument> obj = INSTRUMENTS.register(entry.getId(), entry.getSupplier());
+            entry.setSupplier(obj);
+        }
+        INSTRUMENTS.register(bus);
+    }
+
+    private static void onBuildCreativeTabs(BuildCreativeModeTabContentsEvent event) {
+        BignayItems.addItemsToTabs(event.getTabKey(), event);
+
+        if (event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
+            BignayInstruments.COPPER_HORNS.forEach(entry -> {
+                Instrument instrument = entry.get();
+                ResourceLocation res = BuiltInRegistries.INSTRUMENT.getKey(instrument);
+
+                if (res != null && !res.getPath().equals("empty")) {
+                    ItemStack stack = new ItemStack(BignayItems.COPPER_HORN.get());
+                    CompoundTag nbt = new CompoundTag();
+                    nbt.putString("instrument", res.toString());
+                    stack.setTag(nbt);
+                    event.accept(stack);
+                }
+            });
+        }
     }
 }
