b5413bb81710604d241c94ad088c022595de06d8
Michał Hrycek <kotyk9921@gmail.com>
2026-06-05 18:46:07 +0200

    Add forge support
    Add forge support

--- prepare.sh
+++ prepare.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
+rm -fr forge/build/*
+
 ./gradlew :fabric:runClient &
 PROCESS=$!
 sleep 15
 kill $PROCESS
-
-#./gradlew :forge:runClient &
-#PROCESS=$!
-#sleep 15
-#kill $PROCESS
 
 ./gradlew :neoforge:runClient &
 PROCESS=$!
@@ -16,6 +13,10 @@ sleep 15
 kill $PROCESS
 
 ./gradlew clean build
+
+cd /home/mysiupysiu/Bignay/forge
+./gradlew build jar
+cd ..
 
 rm -fr build/*
 
@@ -25,5 +26,4 @@ cp neoforge/build/libs/*-neoforge.jar build
 
 rm -fr common/run
 rm -fr fabric/run
-rm -fr forge/run
 rm -fr neoforge/run
